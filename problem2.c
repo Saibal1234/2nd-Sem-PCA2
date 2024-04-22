448e7f6fa2f69da791c821c8a5824ccf2ade27c7
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-04-23 01:47:19 +0530

    2nd problem

---
+++ problem2.c
@@ -0,0 +1,10 @@
+// WACP to add two numbers. 
+#include<stdio.h> 
+int main() 
+{ 
+ int a, b; 
+ printf("Enter two numbers to get sum : "); 
+ scanf("%d%d", &a, &b); 
+ printf("The sum is %d", a+b); 
+ return 0; 
+} 
