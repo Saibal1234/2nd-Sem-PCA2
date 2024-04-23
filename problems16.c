aa42c73c987f4b6edd6b83f0da8172452286a406
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-04-23 20:59:13 +0530

    problem

---
+++ problems16.c
@@ -0,0 +1,16 @@
+//  WACP to check whether a number is odd or even. (using simple if 
+// statement) 
+#include <stdio.h> 
+ 
+int main() { 
+ printf("enter a number:"); 
+ int n; 
+ scanf("%d",&n); 
+ if(n%2==0){ 
+ printf("it is an even number"); 
+ } 
+ else{ 
+ printf("it is an odd number"); 
+ } 
+ return 0; 
+} 
