c5c51a5ab05a1e5e5e73762eea685af6c0eb939c
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-04-23 09:32:03 +0530

    3rd problem

---
+++ problem3.c
@@ -0,0 +1,10 @@
+//  WACP to read a character and print its ASCII value.
+#include<stdio.h> 
+int main() 
+{ 
+ char a; 
+ printf("Enter a character to get its ASCII value : "); 
+ scanf("%c", &a); 
+ printf("ASCII value is %d", a); 
+ return 0; 
+} 
